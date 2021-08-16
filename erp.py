2487729eb02f22ba3abcead2e7584aab73fb438f
sachin <sachincsjb@gmail.com>
2021-08-16 09:13:55 +0530

    new functionality adding manage organizations

--- erp.py
+++ erp.py
@@ -1,6 +1,7 @@
 print("-------------------ERP-----------------")
 employee = {}
 teams = {}
+organization = {}
 
 
 def erp_menu():
@@ -10,8 +11,59 @@ def erp_menu():
     print("4.Change Employee Data")
     print("5.Display Employee data : ")
     print("6.Manage all Teams ")
+    print("7.manage organization")
     print("6.exit")
 #    choice = int(input("Enter your choice :"))
+def manage_organization():
+    print("1.Add organization")
+    print("2.edit organization")
+    print("3.display organization")
+    print("4.exit")
+
+
+
+def manage_all_organization():
+    manage_organization()
+    while True:
+        
+        ch = int(input("\ttenter your choice : "))
+        if ch == 1:
+
+            emp_id= input("\tEnter your employee id :")
+            if emp_id in employee.keys():
+
+                org_name = input("\tEnter thr organization")
+                org_email = input("\tEnter the organization email :")
+                temp = {
+                    "org_name":org_name,
+                    "org_email":org_email
+
+                    }
+                organization[emp_id] = temp
+            else:
+                print("employee id is wrong ")
+        elif ch == 2:
+            emp_id = input("\tEnter employee no :")
+            if emp_id not in employee.keys():
+                print("\twrong employee id")
+            else:
+                organization[emp_id]['org_name'] = input("\tEnter the new organization name :")
+                display_organization()
+
+        elif ch == 3:
+            display_organization()
+        elif ch == 4:
+            break;
+        else:
+            print("invalid choice")
+
+        
+
+
+def display_organization():
+    for empid,org_details in organization.items():
+        print(f"\t{empid} | {org_details['org_name']} | {org_details['org_email']} ")
+
 
 
 
@@ -47,6 +99,8 @@ def delete_employee():
     else:
         del employee[emp_id]
 
+
+      
 
 def search_employee_name():
     name = input("\tEnter the name you want search :")
@@ -211,6 +265,9 @@ while True :
         print("--------Manage all Teams-----")
         managa_all_teams()
     elif ch == 7:
+        print("--------manage organization-----")
+        manage_all_organization()
+    elif ch == 8:
         break;
     else:
         print("Invalid Choice")
